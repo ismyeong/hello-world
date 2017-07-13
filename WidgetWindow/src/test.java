39f13c149c0ecbca7a346493ef987de9fd89b9c9
ismyeong <ismyeong@pnpsecure.com>
2017-07-13 09:43:45 +0900

    3번째

--- WidgetWindow/src/test.java
+++ WidgetWindow/src/test.java
@@ -1,4 +1,56 @@
 public class test {
 	public static void main(String[] args) {
+		System.out.println("HelloWorld");	
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
 	}
 }
