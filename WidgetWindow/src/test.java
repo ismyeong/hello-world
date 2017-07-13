9ac4999ab3fbce92d79bc519df38aee73b0c93bb
ismyeong <ismyeong@pnpsecure.com>
2017-07-13 09:11:00 +0900

    2. TEST COMMIT

--- WidgetWindow/src/test.java
+++ WidgetWindow/src/test.java
@@ -1,5 +1,8 @@
 public class test {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld");		
 	}
 }
