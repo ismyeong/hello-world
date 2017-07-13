74dc07a664341c46e154c29008e7ee2d364fe9db
ismyeong <ismyeong@pnpsecure.com>
2017-07-13 09:49:52 +0900

    1

--- WidgetWindow/src/test.java
+++ WidgetWindow/src/test.java
@@ -1,11 +1,5 @@
 public class test {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");	
-		System.out.println("HelloWorld");		
-		System.out.println("HelloWorld");
-		System.out.println("HelloWorld");		
-		System.out.println("HelloWorld");		
-		System.out.println("HelloWorld");		
-		System.out.println("HelloWorld");		
+		System.out.println("HelloWorld1");
 	}
 }
